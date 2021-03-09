b29cb689606d4d4546274fb66ff04cc5f25f3f1b
minlaxz <minminlaxz@gmail.com>
2021-03-09 18:23:21 +0630

    used viewsetfor routers

--- apis/views.py
+++ apis/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+# from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.authentication import TokenAuthentication
 # Create your views here.
+from todos import models
+from .serializers import TodoSerializer
+
+class TodoViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    authentication_classes = [TokenAuthentication]
+    queryset = models.Todo.objects.all().order_by('id')
+    serializer_class = TodoSerializer
