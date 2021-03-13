5d843664dda3dcf5430657efab1919ced24938d6
minlaxz <minminlaxz@gmail.com>
2021-03-13 22:25:47 +0630

    added 'completed' field to serializer

--- apis/serializers.py
+++ apis/serializers.py
@@ -1,8 +1,7 @@
-from django.db.models import fields
 from rest_framework import serializers
 from todos.models import Todo
 
 class TodoSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Todo
-        fields = ('id', 'title', 'description', 'created_at')
+        fields = ('id', 'title', 'description', 'created_at', 'completed')
